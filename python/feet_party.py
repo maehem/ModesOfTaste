20ba8e69637448e905804afb9fd00ef39046b46b
maehem <maehem@users.noreply.github.com>
2017-07-25 22:59:52 -0700

    Noodle can now determine his geolocation using IP address.

--- python/feet_party.py
+++ python/feet_party.py
@@ -5,6 +5,11 @@ import datetime
 import serial
 from picamera import PiCamera
 from time import sleep
+from geolocation.main import GoogleMaps
+from geolocation.distance_matrix.client import DistanceMatrixApiClient
+import urllib2
+import json
+
 
 camera = PiCamera()
 
@@ -20,12 +25,48 @@ def takePicture():
 	sleep(2)
 	camera.capture('/home/pi/Desktop/' + timeStamp() + '-snap.jpg')
 	camera.stop_preview()
+	ser.write('<tick>')
 
 def taste(val):
 	# log the color sensor value from the taste command
 	ser.write('<lick>')
 	f=open("/home/pi/Desktop/tasteLog.txt","a+")
 	f.write(timeStamp() + ':' + val + '\n')
+
+def whereAreMyFeet():
+	f=open("~/google-map-api-key.txt", "r")
+	api_key=f.readline()
+	google_maps = GoogleMaps(api_key)
+
+	#location = google_maps.search(location=address) # sends search to Google Maps.
+	location = google_maps.search() # sends search to Google Maps.
+
+	print(location.all()) # returns all locations.
+
+	my_location = location.first() # returns only first location.
+
+	print(my_location.city)
+	print(my_location.route)
+	print(my_location.street_number)
+	print(my_location.postal_code)
+
+def whereAreMyFeet2():
+	f=urllib2.urlopen("https://api.ipify.org/?format=json")
+	json_string = f.read()
+	f.close()
+	stuff = json.loads(json_string)
+	ip = stuff['ip']
+	# Automatically geolocate the connecting IP
+	f = urllib2.urlopen('http://freegeoip.net/json/'+ip)
+	json_string = f.read()
+	f.close()
+	location = json.loads(json_string)
+	#print(location)
+	location_city = location['city']
+	#location_state = location['region_name']
+	#location_country = location['country_name']
+	#location_zip = location['zip_code']
+	ser.write('\n\r' + location['city'])
 
 
 def prompt():
@@ -72,12 +113,13 @@ while 1:
 		# if the command is 'snap' then call takePicture
 		if word[0]=='snap':
 			takePicture()
-			ser.write('\n<tick>')
 		if word[0]=='taste':
 			if len(word) > 1:
 				taste(word[1])
 			else:
 				serial.write('error: no value for tasting!')
+		if word[0]=='locate':
+			whereAreMyFeet2()
 
 
 		prompt()
